4fdcdcd022e034800e162375398eb0e96ae3f6a8
Muhammad-Nouman-690 <nomi690@hotmail.com>
2020-08-12 01:48:34 +0500

    To-do Application

---
+++ app.js
@@ -0,0 +1,45 @@
+var list = document.getElementById('list')
+
+function addTodo(){
+    var todoItem = document.getElementById('todo-item')
+    
+    // List
+    var li = document.createElement('li')
+    var liText = document.createTextNode(todoItem.value)
+    li.appendChild(liText)
+    
+   
+
+    // edit
+    var editBtn = document.createElement('button')
+    var editText = document.createTextNode('Edit')
+    editBtn.appendChild(editText)
+    editBtn.setAttribute("class","btn btn-outline-secondary")
+    editBtn.setAttribute("onclick","editItem(this)")
+    li.appendChild(editBtn)
+
+     // Delete
+     var delBtn = document.createElement('button')
+     var delText = document.createTextNode('Delete')
+     delBtn.appendChild(delText)
+     delBtn.setAttribute("class","btn btn-outline-secondary")
+     delBtn.setAttribute("onclick","delItem(this)")
+     li.appendChild(delBtn)
+
+    list.appendChild(li)
+    todoItem.value = ""
+
+}
+
+function delItem(e){
+    e.parentNode.remove(e)
+}
+
+function editItem(e){
+    var val = e.parentNode.firstChild.nodeValue;
+    e.parentNode.firstChild.nodeValue = prompt("Enter Value",val)   
+}
+
+function deleAll(){
+    list.innerHTML = ""
+}
